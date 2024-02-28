9592e1bdec2f20ea8aa6fe0dff040d0f093d4164
oluwaferanmi <feranmijogbodo@gmail.com>
2024-02-28 17:54:31 +0100

    Add OTP input field validation and handling

--- app/(auth)/otp/page.tsx
+++ app/(auth)/otp/page.tsx
@@ -1,35 +1,96 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Row, Col } from "antd";
 import Image from "next/image";
 import { AuthFormWrapper } from "@/components/molecules/wrappers";
 import { FormHeaderText } from "@/components/atoms/texts";
 import { FormSubmitButton } from "@/components/atoms/inputs";
+import { toast } from "sonner";
 
 const OtpPage = () => {
   const [otpVal, setOtpValue] = useState(["", "", "", "", "", ""]);
   const [bLoading, setBLoading] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleUpdateIndex = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    if (isNaN(+e.target.value)) {
+      // Do not forget an empty space using the + is still implicitly a number
+      return;
+    }
+    const otpSnap = [...otpVal];
+    otpSnap[index] = e.target.value.substring(e.target.value.length - 1);
+    setOtpValue([...otpSnap]);
+    if (e.target.value === "") {
+      setActiveIndex(index);
+      return;
+    }
+    setActiveIndex(index + 1);
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+    return () => {
+      setActiveIndex(-1);
+    };
+  }, [activeIndex]);
+
+  const handleOnKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const otpValSnap = [...otpVal];
+    otpValSnap[index] = !isNaN(+e.key) ? e.key : otpValSnap[index];
+    if (otpValSnap[index] === "" && e.key === "Backspace") {
+      setActiveIndex(index - 1);
+    }
+  };
+
+  const handleSubmitOtp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setBLoading(true);
+    // convert otpVal to string , then check the lenght
+    const newString = otpVal.join("");
+    console.log(newString);
+
+    if (newString.length < 6) {
+      toast.error("Please enter OTP code correctly");
+      setBLoading(false);
+      return;
+    }
+    console.log(otpVal);
+  };
   return (
     <AuthFormWrapper>
       <div className="mb-8">
         <FormHeaderText text="OTP" />
       </div>
       <p className="my-2">Secure code</p>
-      <div className="flex items-center gap-3">
-        {otpVal.map((item, index) => {
-          return (
-            <div>
-              <input
-                placeholder="0"
-                className="w-[60px] h-[60px] flex items-center justify-center focus:outline-none focus:border-hgrey100 px-3 text-3xl text-center rounded-lg border-2 border-hgrey50"
-              />
-            </div>
-          );
-        })}
-      </div>
-      <p className="text-hgrey200 my-2">This is a hint text to help user.</p>
-      <FormSubmitButton disabled={bLoading} text="Verify" />
+      <form onSubmit={handleSubmitOtp}>
+        <div className="flex items-center gap-3">
+          {otpVal.map((item, index) => {
+            return (
+              <div key={index}>
+                <input
+                  onKeyDown={(e) => {
+                    handleOnKeyDown(e, index);
+                  }}
+                  ref={activeIndex == index ? inputRef : null}
+                  placeholder="0"
+                  value={otpVal[index]}
+                  onChange={(e) => handleUpdateIndex(e, index)}
+                  className="w-[60px] h-[60px] flex items-center justify-center focus:outline-none focus:border-hgrey100 px-3 text-3xl text-center rounded-lg border-2 border-hgrey50"
+                />
+              </div>
+            );
+          })}
+        </div>
+        <p className="text-hgrey200 my-2">This is a hint text to help user.</p>
+        <FormSubmitButton disabled={bLoading} text="Verify" />
+      </form>
     </AuthFormWrapper>
   );
 };
